a4fbfc4a0a7fbcb3a7ff75cb3bb22d9ca9d5d4f9
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-09-19 08:21:37 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,10 @@
+const country = 'bangladesh';
+const age = 52;
+const bondu = {id:12121, name: 'korim', age:22};
+const friends = ['sayed', 'alfin', 'anayet','abid'];
+console.log(typeof country);
+console.log(typeof age);
+console.log(typeof bondu);
+console.log(Array.isArray(friends)); //for check aaray
+console.log(friends.includes('anayet')); //any item search in array
+
